e3a327e6dbbacccf10750041eaf7752caefffc9d
distoritons81 <carlotto81@gmail.com>
2024-04-27 00:36:39 -0600

    add echo/comment to cert gen script

--- makeTestCert.sh
+++ makeTestCert.sh
@@ -2,3 +2,5 @@
 
 openssl ecparam -genkey -name prime256v1 -out data/server.key
 openssl req -new -x509 -key server.key -out data/server.pem -days 3650
+echo "Generated."
+echo "If you have a domain name, use letsEncrypt instead!"
