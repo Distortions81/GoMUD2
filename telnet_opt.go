c2a7f7e814e6aac5b5422f5a713d049feb770e6d
distortions81 <carlotto81@gmail.com>
2024-04-28 21:51:51 -0600

    .

--- telnet_opt.go
+++ telnet_opt.go
@@ -22,7 +22,8 @@ var termTypeMap map[string]*termSettings = map[string]*termSettings{
 	"KBTIN": {ANSI256: true, UTF: true},
 	//mudlet wont/dont sup_gohead, do charset
 	"MUDLET": {ANSI256: true, UTF: true},
-	//mudmagic WONT TERMINAL_TYPE, Accepts but does not send UTF-8, and does not accept latin1
+	//mudmagic WONT TERMINAL_TYPE, sup_goahead. DONT charset, supgohead.
+	//will accept but does not send UTF-8, does not accept latin1
 	"MUDMAGIC":      {ANSI256: false, UTF: true},
 	"MUSHCLIENT":    {ANSI256: true, UTF: true},
 	"POTATO":        {ANSI256: true, UTF: true},
