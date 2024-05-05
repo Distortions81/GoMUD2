d1ef704fcbe29e5e4f189adfc6d6e94c68bccbbe
distortions81 <carlotto81@gmail.com>
2024-05-05 10:58:37 -0600

    main loop cleanup

--- mainLoop.go
+++ mainLoop.go
@@ -24,66 +24,77 @@ func mainLoop() {
 
 		descLock.Lock()
 		hashReceiver()
-
-		//Remove dead characters
-		var newCharacterList []*characterData
-		for _, target := range characterList {
-			if !target.valid {
-				target.sendToPlaying("%v slowly fades away.", target.Name)
-				errLog("Removed character %v", target.Name)
-				continue
-			} else if time.Since(target.idleTime) > CHARACTER_IDLE {
-				target.send("Idle too long, quitting...")
-				target.quit(true)
-				continue
-			}
-			newCharacterList = append(newCharacterList, target)
-		}
-		characterList = newCharacterList
-
-		//Remove dead descriptors
-		var newDescList []*descData
-		for _, desc := range descList {
-			if desc.state == CON_LOGIN &&
-				time.Since(desc.idleTime) > LOGIN_AFK {
-				desc.sendln("\r\nIdle too long, disconnecting.")
-				desc.killDesc()
-				continue
-			} else if desc.state != CON_PLAYING &&
-				time.Since(desc.idleTime) > AFK_DESC {
-				desc.sendln("\r\nIdle too long, disconnecting.")
-				desc.killDesc()
-				continue
-			} else if desc.state == CON_DISCONNECTED || !desc.valid {
-				desc.killDesc()
-				continue
-			}
-			newDescList = append(newDescList, desc)
-		}
-		descList = newDescList
-
-		//Interpret all
-		for _, desc := range descList {
-			desc.interp()
-		}
-
-		//Shuffle descriptor list
-		numDesc := len(descList) - 1
-		if numDesc > 1 {
-			j := rand.Intn(numDesc) + 1
-			descList[0], descList[j] = descList[j], descList[0]
-		}
+		removeDeadChar()
+		removeDeadDesc()
+		interpAllDesc()
+		descShuffle()
 		descLock.Unlock()
 
 		//Sleep for remaining round time
 		timeLeft := roundTime - time.Since(start)
-
 		if timeLeft <= 0 {
-			errLog("Round went over: %v", time.Duration(timeLeft).Round(time.Microsecond).Abs().String())
+			critLog("Round went over: %v", time.Duration(timeLeft).Round(time.Microsecond).Abs().String())
 		} else {
 			time.Sleep(timeLeft)
 		}
 	}
+}
+
+func descShuffle() {
+	//Shuffle descriptor list
+	numDesc := len(descList) - 1
+	if numDesc > 1 {
+		j := rand.Intn(numDesc) + 1
+		descList[0], descList[j] = descList[j], descList[0]
+	}
+}
+
+func interpAllDesc() {
+	//Interpret all
+	for _, desc := range descList {
+		desc.interp()
+	}
+}
+
+func removeDeadChar() {
+	//Remove dead characters
+	var newCharacterList []*characterData
+	for _, target := range characterList {
+		if !target.valid {
+			target.sendToPlaying("%v slowly fades away.", target.Name)
+			errLog("Removed character %v", target.Name)
+			continue
+		} else if time.Since(target.idleTime) > CHARACTER_IDLE {
+			target.send("Idle too long, quitting...")
+			target.quit(true)
+			continue
+		}
+		newCharacterList = append(newCharacterList, target)
+	}
+	characterList = newCharacterList
+}
+
+func removeDeadDesc() {
+	//Remove dead descriptors
+	var newDescList []*descData
+	for _, desc := range descList {
+		if desc.state == CON_LOGIN &&
+			time.Since(desc.idleTime) > LOGIN_AFK {
+			desc.sendln("\r\nIdle too long, disconnecting.")
+			desc.killDesc()
+			continue
+		} else if desc.state != CON_PLAYING &&
+			time.Since(desc.idleTime) > AFK_DESC {
+			desc.sendln("\r\nIdle too long, disconnecting.")
+			desc.killDesc()
+			continue
+		} else if desc.state == CON_DISCONNECTED || !desc.valid {
+			desc.killDesc()
+			continue
+		}
+		newDescList = append(newDescList, desc)
+	}
+	descList = newDescList
 }
 
 func (desc *descData) killDesc() {
