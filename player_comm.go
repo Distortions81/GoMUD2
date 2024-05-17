79aeac13582b6a87943c816ba6b63ca76640e45f
Distortions81 <carlotto81@gmail.com>
2024-05-17 13:07:44 -0600

    .

--- player_comm.go
+++ player_comm.go
@@ -13,15 +13,15 @@ type chanData struct {
 
 // Do not change channel IDs, max 63
 var channels []*chanData = []*chanData{
-	0: {Name: "Implementor", Short: "Imp", Desc: "Implementor chat", Format: "[IMP] %v says: %v", Level: LEVEL_IMPLEMENTOR},
-	1: {Name: "Administrator", Short: "Admin", Desc: "Administrator chat", Format: "[ADMIN] %v says: %v", Level: LEVEL_ADMIN},
-	2: {Name: "Builder", Short: "Build", Desc: "Builder chat", Format: "[BUILDER] %v says: %v", Level: LEVEL_BUILDER},
-	3: {Name: "Staff", Short: "Staff", Desc: "Chat for all staff", Format: "[STAFF] %v says: %v", Level: LEVEL_MODERATOR},
-	4: {Name: "Moderation", Short: "Mod", Desc: "Moderatorion Request", Format: "[MOD] %v says: %v", Level: LEVEL_ANY},
-	5: {Name: "Announce", Short: "Announce", Desc: "Official Announcements", Format: "[Announcement] %v says: %v", Level: LEVEL_ADMIN},
-	6: {Name: "Congrats", Short: "Grats", Desc: "Congratulate someone!", Format: "[Grats] %v says: %v", Level: LEVEL_PLAYER},
-	7: {Name: "Newbie", Short: "Newb", Desc: "A place for newbies to chat or ask for help", Format: "[Newbie] %v says: %v", Level: LEVEL_NEWBIE},
-	8: {Name: "OOC", Short: "OOC", Desc: "out-of-character chat", Format: "[OOC] %v says: %v", Level: LEVEL_NEWBIE},
+	0: {Name: "Implementor", Short: "Imp", Desc: "Implementor chat", Format: "[IMP] %v: %v", Level: LEVEL_IMPLEMENTOR},
+	1: {Name: "Administrator", Short: "Admin", Desc: "Administrator chat", Format: "[ADMIN] %v: %v", Level: LEVEL_ADMIN},
+	2: {Name: "Builder", Short: "Build", Desc: "Builder chat", Format: "[BUILDER] %v: %v", Level: LEVEL_BUILDER},
+	3: {Name: "Staff", Short: "Staff", Desc: "Chat for all staff", Format: "[STAFF] %v: %v", Level: LEVEL_MODERATOR},
+	4: {Name: "Moderation", Short: "Mod", Desc: "Moderatorion Request", Format: "[MOD] %v: %v", Level: LEVEL_ANY},
+	5: {Name: "Announce", Short: "Announce", Desc: "Official Announcements", Format: "[Announcement] %v: %v", Level: LEVEL_ADMIN},
+	6: {Name: "Congrats", Short: "Grats", Desc: "Congratulate someone!", Format: "[Grats] %v: %v", Level: LEVEL_PLAYER},
+	7: {Name: "Newbie", Short: "Newb", Desc: "A place for newbies to chat or ask for help", Format: "[Newbie] %v: %v", Level: LEVEL_NEWBIE},
+	8: {Name: "OOC", Short: "OOC", Desc: "out-of-character chat", Format: "[OOC] %v: %v", Level: LEVEL_NEWBIE},
 }
 
 func sendToChannel(player *characterData, input string, channel Bitmask) {
@@ -31,9 +31,17 @@ func sendToChannel(player *characterData, input string, channel Bitmask) {
 		critLog("sendToChannel: Player %v tried to use an invalid chat channel: %v", player.Name, channel)
 		return
 	}
+	if player.Channels.HasFlag(channel) {
+		player.send("You currently have the %v channel turned off.", chd.Name)
+		return
+	}
 	for _, target := range charList {
 		if !target.Channels.HasFlag(channel) {
-			target.send(chd.Format, player.Name, input)
+			if target == player {
+				target.send(chd.Format, "You", input)
+			} else {
+				target.send(chd.Format, player.Name, input)
+			}
 		}
 	}
 }
