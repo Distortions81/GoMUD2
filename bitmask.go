92b01ca460f211946407259a6b304d1f3683e826
distoritons81 <carlotto81@gmail.com>
2024-05-21 01:19:46 -0600

    convert bitmasks to variable-length base64

--- bitmask.go
+++ bitmask.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/binary"
 	"encoding/json"
+	"fmt"
+	"strings"
 )
+
+const BITMASK_STRING_BASE = 10
 
 type Bitmask uint64
 
@@ -14,31 +15,47 @@ func (f *Bitmask) AddFlag(flag Bitmask)     { *f |= flag }
 func (f *Bitmask) ClearFlag(flag Bitmask)   { *f &= ^flag }
 func (f *Bitmask) ToggleFlag(flag Bitmask)  { *f ^= flag }
 
-// Implementing json.Marshaler interface for Bitmask
+const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
 func (b Bitmask) MarshalJSON() ([]byte, error) {
-	var buf bytes.Buffer
-	err := binary.Write(&buf, binary.BigEndian, b)
-	if err != nil {
-		return nil, err
-	}
-	encoded := base64.StdEncoding.EncodeToString(buf.Bytes())
-	return json.Marshal(encoded)
+	base64String := uint64ToBase64(uint64(b))
+	return json.Marshal(base64String)
 }
 
-// Implementing json.Unmarshaler interface for Bitmask
 func (b *Bitmask) UnmarshalJSON(data []byte) error {
-	var encoded string
-	if err := json.Unmarshal(data, &encoded); err != nil {
+	var base64String string
+	if err := json.Unmarshal(data, &base64String); err != nil {
 		return err
 	}
-	decoded, err := base64.StdEncoding.DecodeString(encoded)
+
+	num, err := base64ToUint64(base64String)
 	if err != nil {
 		return err
 	}
-	buf := bytes.NewReader(decoded)
-	err = binary.Read(buf, binary.BigEndian, b)
-	if err != nil {
-		return err
-	}
+
+	*b = Bitmask(num)
 	return nil
+}
+
+func uint64ToBase64(num uint64) string {
+	var result string
+	for num > 0 {
+		index := num & 0x3f // Mask to get the last 6 bits
+		result = string(base64Chars[index]) + result
+		num >>= 6 // Shift right by 6 bits
+	}
+	return result
+}
+
+func base64ToUint64(base64String string) (uint64, error) {
+	var num uint64
+	for i := 0; i < len(base64String); i++ {
+		char := base64String[i]
+		index := strings.IndexByte(base64Chars, char)
+		if index == -1 {
+			return 0, fmt.Errorf("invalid base64 character: %c", char)
+		}
+		num = (num << 6) | uint64(index) // Shift left by 6 bits and add index
+	}
+	return num, nil
 }
