16961d6ff2888dac7126347734f52a070a2644d0
Distortions81 <carlotto81@gmail.com>
2024-06-05 20:25:42 -0600

    fix test cert gen

--- makeTestCert.sh
+++ makeTestCert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl ecparam -genkey -name prime256v1 -out data/privkey.pem
-openssl req -new -x509 -key data/privkey.pem -out data/cert.pem -days 3650
+openssl ecparam -genkey -name prime256v1 -out privkey.pem
+openssl req -new -x509 -key privkey.pem -out fullchain.pem -days 3650
 echo "Generated."
 echo "If you have a domain name, use letsEncrypt instead!"
