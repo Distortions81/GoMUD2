b0b9bb4dfe67f6b458ab13a904d2965886099369
Distortions81 <carlotto81@gmail.com>
2024-06-09 13:35:33 -0600

    .

--- color.go
+++ color.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -11,19 +10,6 @@ const ANSI_ESC = "\033["
 const ANSI_RESET = ANSI_ESC + "m"
 const NEWLINE = "\r\n"
 
-var extendedTable map[int]*ctData
-
-func init() {
-	extendedTable = make(map[int]*ctData)
-	for x := 17; x < 256; x++ {
-		extendedTable[x] = &ctData{code: "38;5;" + strconv.FormatInt(int64(x), 10), isFG: true, isBG: false}
-	}
-	offset := 300
-	for x := 17; x < 256; x++ {
-		extendedTable[x+offset] = &ctData{code: "48;5;" + strconv.FormatInt(int64(x), 10), isFG: false, isBG: true}
-	}
-}
-
 func xcolorHelp() {
 	var outbuf string
 	var lineBuf []string
@@ -31,7 +17,7 @@ func xcolorHelp() {
 	for _, line := range colorSwatch {
 		buf := ""
 		for _, color := range line {
-			buf = buf + fmt.Sprintf("[%03v%03v ", color, color)
+			buf = buf + fmt.Sprintf("{%03v%03v ", color, color)
 		}
 		lineBuf = append(lineBuf, buf)
 	}
@@ -39,24 +25,24 @@ func xcolorHelp() {
 	for i, line := range colorPastelSwatch {
 		buf := " "
 		for _, color := range line {
-			buf = buf + fmt.Sprintf("[%03v%03v ", color, color)
+			buf = buf + fmt.Sprintf("{%03v%03v ", color, color)
 		}
 		lineBuf[i] = lineBuf[i] + buf
 	}
 	outbuf = outbuf + strings.Join(lineBuf, NEWLINE)
 	outbuf = outbuf + NEWLINE
-	outbuf = outbuf + "[xGrayscale:" + NEWLINE
+	outbuf = outbuf + "{xGrayscale:" + NEWLINE
 	for _, line := range graySwatch {
 		buf := " "
 		for _, color := range line {
-			buf = buf + fmt.Sprintf("[%03v%03v ", color, color)
+			buf = buf + fmt.Sprintf("{%03v%03v ", color, color)
 		}
 		outbuf = outbuf + buf
 	}
 	outbuf = outbuf + NEWLINE
-	outbuf = outbuf + "[xSyntax: [[088[088Hello[x[[x. Requires 3 digits, padded with 0s if needed."
+	outbuf = outbuf + "{xSyntax: {{088{088Hello{x{{x. Requires 3 digits, padded with 0s if needed."
 	outbuf = outbuf + NEWLINE
-	outbuf = outbuf + "Background colors: add 300 to the number: [[388[388Hello[x[[x."
+	outbuf = outbuf + "Background colors: add 300 to the number: {{388{388Hello{x{{x."
 
 	for _, file := range helpFiles {
 		if strings.EqualFold(file.Topic, "basics") {
@@ -64,11 +50,11 @@ func xcolorHelp() {
 			newHelp := helpData{Name: "xcolor", Keywords: []string{"ANSI", "color", "extended", "256"}, Authors: []string{"System"}, Text: outbuf, topic: file}
 
 			//Update if found, otherwise create
-			for _, help := range file.Helps {
+			for h, help := range file.Helps {
 				if strings.EqualFold(help.Name, "xcolor") {
 					newHelp.Created = time.Now().UTC()
 					newHelp.Modified = time.Now().UTC()
-					help = newHelp
+					file.Helps[h] = newHelp
 					return
 				}
 			}
@@ -82,38 +68,38 @@ func xcolorHelp() {
 
 var colorTable map[byte]*ctData = map[byte]*ctData{
 	//bg colors
-	'0': {code: "40", isBG: true}, //black
-	'1': {code: "41", isBG: true}, //red
-	'2': {code: "42", isBG: true}, //green
-	'3': {code: "43", isBG: true}, //yellow
-	'4': {code: "44", isBG: true}, //blue
-	'5': {code: "45", isBG: true}, //magenta
-	'6': {code: "46", isBG: true}, //cyan
-	'7': {code: "47", isBG: true}, //white
+	'!': {code: "40", isBG: true}, //black
+	'@': {code: "41", isBG: true}, //red
+	'#': {code: "42", isBG: true}, //green
+	'$': {code: "43", isBG: true}, //yellow
+	'%': {code: "44", isBG: true}, //blue
+	'^': {code: "45", isBG: true}, //magenta
+	'&': {code: "46", isBG: true}, //cyan
+	'*': {code: "47", isBG: true}, //white
 
-	'k': {code: "30", isFG: true, removeBold: true}, //black
-	'r': {code: "31", isFG: true, removeBold: true}, //red
-	'g': {code: "32", isFG: true, removeBold: true}, //green
-	'y': {code: "33", isFG: true, removeBold: true}, //yellow
-	'b': {code: "34", isFG: true, removeBold: true}, //blue
-	'm': {code: "35", isFG: true, removeBold: true}, //magenta
-	'c': {code: "36", isFG: true, removeBold: true}, //cyan
-	'w': {code: "37", isFG: true, removeBold: true}, //white
+	'k': {code: "30", isFG: true}, //black
+	'r': {code: "31", isFG: true}, //red
+	'g': {code: "32", isFG: true}, //green
+	'y': {code: "33", isFG: true}, //yellow
+	'b': {code: "34", isFG: true}, //blue
+	'm': {code: "35", isFG: true}, //magenta
+	'c': {code: "36", isFG: true}, //cyan
+	'w': {code: "37", isFG: true}, //white
 
-	'K': {code: "30", isFG: true, style: bold}, //black
-	'R': {code: "31", isFG: true, style: bold}, //red
-	'G': {code: "32", isFG: true, style: bold}, //green
-	'Y': {code: "33", isFG: true, style: bold}, //yellow
-	'B': {code: "34", isFG: true, style: bold}, //blue
-	'M': {code: "35", isFG: true, style: bold}, //magenta
-	'C': {code: "36", isFG: true, style: bold}, //cyan
-	'W': {code: "37", isFG: true, style: bold}, //white
+	'K': {code: "90", isFG: true}, //black
+	'R': {code: "91", isFG: true}, //red
+	'G': {code: "92", isFG: true}, //green
+	'Y': {code: "93", isFG: true}, //yellow
+	'B': {code: "94", isFG: true}, //blue
+	'M': {code: "95", isFG: true}, //magenta
+	'C': {code: "96", isFG: true}, //cyan
+	'W': {code: "97", isFG: true}, //white
 
-	'!': {code: "1", disCode: "22", isStyle: true, style: bold},
-	'*': {code: "3", disCode: "23", isStyle: true, style: italic},
+	'+': {code: "1", disCode: "22", isStyle: true, style: bold},
+	'/': {code: "3", disCode: "23", isStyle: true, style: italic},
 	'_': {code: "4", disCode: "24", isStyle: true, style: underline},
-	'^': {code: "7", disCode: "27", isStyle: true, style: inverse},
-	'~': {code: "9", disCode: "29", isStyle: true, style: strike},
+	'=': {code: "7", disCode: "27", isStyle: true, style: inverse},
+	'-': {code: "9", disCode: "29", isStyle: true, style: strike},
 }
 
 func ColorRemove(in []byte) []byte {
@@ -175,96 +161,9 @@ func ANSIColor(in []byte, colorMode int) []byte {
 	//Process ANSI 256 color
 	out = []byte{}
 	inLen = len(in)
-	var ext string
+	var ext []byte
 	for x := 0; x < inLen; x++ {
 
-		//Extended color
-		if in[x] == '[' {
-			x++
-			if x < (inLen - 3) {
-				if in[x] == '[' {
-					out = append(out, '[')
-					continue
-				} else if in[x] == 'x' {
-					if s.hasColor {
-						s.resetState()
-
-						out = append(out, []byte(ANSI_RESET)...)
-					}
-					continue
-				} else if in[x] < '0' || in[x] > '9' {
-					out = append(out, []byte{'[', in[x]}...)
-					continue
-				}
-				ext = ""
-				if in[x] >= '0' && in[x] <= '9' {
-					ext = ext + string(in[x])
-					x++
-					if in[x] >= '0' && in[x] <= '9' {
-						ext = ext + string(in[x])
-						x++
-
-						if in[x] >= '0' && in[x] <= '9' {
-							ext = ext + string(in[x])
-						} else {
-							x--
-						}
-					} else {
-						x--
-					}
-				} else {
-					out = append(out, []byte{'[', in[x]}...)
-					continue
-				}
-
-				ival, err := strconv.ParseInt(ext, 10, 64)
-				if err != nil {
-					continue
-				}
-
-				cVal := extendedTable[int(ival)]
-
-				//If redundant, skip
-				if s.lastVal == cVal {
-					continue
-				}
-				s.lastVal = cVal
-
-				//Not a valid code, skips
-				if cVal == nil {
-					continue
-				}
-
-				//If new FG color, set it.
-				if cVal.isFG && s.curColor != cVal.code {
-					if colorMode >= COLOR_256 {
-						s.nextColor = cVal.code
-					} else {
-						s.nextColor = cVal.code
-					}
-				}
-				//If new BG color, set it.
-				if cVal.isBG && s.curBGColor != cVal.code {
-					s.nextBGColor = cVal.code
-				}
-
-				//Toggle styles such as italic
-				if !cVal.isFG {
-					s.nextStyle.toggleFlag(cVal.style)
-				} else {
-					//Otherwise, if bold FG color, add add flag only.
-					s.nextStyle.addFlag(cVal.style)
-				}
-				//If we are switching from bold FG color
-				//to non-bold FG color, remove bold (do not toggle)
-				if cVal.removeBold {
-					s.nextStyle.clearFlag(bold)
-				}
-				continue
-
-			}
-
-		}
 		//Escape code
 		if in[x] == '{' {
 			x++
@@ -283,8 +182,24 @@ func ANSIColor(in []byte, colorMode int) []byte {
 					continue
 				}
 
-				//Look up color/style
-				cVal := colorTable[in[x]]
+				var cVal *ctData
+				//extended color
+				if in[x] >= '0' && in[x] <= '9' {
+					ext = []byte{in[x]}
+					x++
+					if in[x] >= '0' && in[x] <= '9' {
+						ext = append(ext, in[x])
+						x++
+						if in[x] >= '0' && in[x] <= '9' {
+							ext = append(ext, in[x])
+						}
+
+					}
+					cVal = &ctData{isFG: true, extended: true, code: "38;5;" + string(ext)}
+				} else {
+					//Look up color/style
+					cVal = colorTable[in[x]]
+				}
 
 				//If redundant, skip
 				if s.lastVal == cVal {
@@ -340,16 +255,16 @@ func ANSIColor(in []byte, colorMode int) []byte {
 					cOut = append(cOut, "0"...)
 				} else if s.nextStyle.hasFlag(bold) && !s.curStyle.hasFlag(bold) {
 					//Add bold style
-					cOut = append(cOut, colorTable['!'].code...)
+					cOut = append(cOut, colorTable['+'].code...)
 				} else if !s.nextStyle.hasFlag(bold) && s.curStyle.hasFlag(bold) {
 					//Remove bold style
-					cOut = append(cOut, colorTable['!'].disCode...)
+					cOut = append(cOut, colorTable['+'].disCode...)
 				} else if s.nextStyle.hasFlag(italic) && !s.curStyle.hasFlag(italic) {
 					//Add italic style
-					cOut = append(cOut, colorTable['*'].code...)
+					cOut = append(cOut, colorTable['/'].code...)
 				} else if !s.nextStyle.hasFlag(italic) && s.curStyle.hasFlag(italic) {
 					//Remove italic style
-					cOut = append(cOut, colorTable['*'].disCode...)
+					cOut = append(cOut, colorTable['/'].disCode...)
 				} else if s.nextStyle.hasFlag(underline) && !s.curStyle.hasFlag(underline) {
 					//Add underline style
 					cOut = append(cOut, colorTable['_'].code...)
@@ -358,16 +273,16 @@ func ANSIColor(in []byte, colorMode int) []byte {
 					cOut = append(cOut, colorTable['_'].disCode...)
 				} else if s.nextStyle.hasFlag(inverse) && !s.curStyle.hasFlag(inverse) {
 					//Add inverse style
-					cOut = append(cOut, colorTable['^'].code...)
+					cOut = append(cOut, colorTable['='].code...)
 				} else if !s.nextStyle.hasFlag(inverse) && s.curStyle.hasFlag(inverse) {
 					//Remove inverse style
-					cOut = append(cOut, colorTable['^'].disCode...)
+					cOut = append(cOut, colorTable['='].disCode...)
 				} else if s.nextStyle.hasFlag(strike) && !s.curStyle.hasFlag(strike) {
 					//Add strike style
-					cOut = append(cOut, colorTable['~'].code...)
+					cOut = append(cOut, colorTable['-'].code...)
 				} else if !s.nextStyle.hasFlag(strike) && s.curStyle.hasFlag(strike) {
 					//Remove strike style
-					cOut = append(cOut, colorTable['~'].disCode...)
+					cOut = append(cOut, colorTable['-'].disCode...)
 				}
 				//Add BG color if state changed
 				if s.nextBGColor != s.curBGColor {
@@ -455,7 +370,7 @@ type ctData struct {
 	style         Bitmask
 
 	isBG, isFG, removeBold,
-	isStyle bool
+	isStyle, extended bool
 }
 
 var colorSwatch [][]int = [][]int{
@@ -515,23 +430,4 @@ var colorPastelSwatch [][]int = [][]int{
 var graySwatch [][]int = [][]int{
 	{232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243},
 	{244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255},
-}
-
-var color256to16 []int = []int{
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-	0, 4, 4, 4, 12, 12, 2, 6, 4, 4, 12, 12, 2, 2, 6, 4,
-	12, 12, 2, 2, 2, 6, 12, 12, 10, 10, 10, 10, 14, 12, 10, 10,
-	10, 10, 10, 14, 1, 5, 4, 4, 12, 12, 3, 8, 4, 4, 12, 12,
-	2, 2, 6, 4, 12, 12, 2, 2, 2, 6, 12, 12, 10, 10, 10, 10,
-	14, 12, 10, 10, 10, 10, 10, 14, 1, 1, 5, 4, 12, 12, 1, 1,
-	5, 4, 12, 12, 3, 3, 8, 4, 12, 12, 2, 2, 2, 6, 12, 12,
-	10, 10, 10, 10, 14, 12, 10, 10, 10, 10, 10, 14, 1, 1, 1, 5,
-	12, 12, 1, 1, 1, 5, 12, 12, 1, 1, 1, 5, 12, 12, 3, 3,
-	3, 7, 12, 12, 10, 10, 10, 10, 14, 12, 10, 10, 10, 10, 10, 14,
-	9, 9, 9, 9, 13, 12, 9, 9, 9, 9, 13, 12, 9, 9, 9, 9,
-	13, 12, 9, 9, 9, 9, 13, 12, 11, 11, 11, 11, 7, 12, 10, 10,
-	10, 10, 10, 14, 9, 9, 9, 9, 9, 13, 9, 9, 9, 9, 9, 13,
-	9, 9, 9, 9, 9, 13, 9, 9, 9, 9, 9, 13, 9, 9, 9, 9,
-	9, 13, 11, 11, 11, 11, 11, 15, 0, 0, 0, 0, 0, 0, 8, 8,
-	8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 15, 15, 15, 15, 15, 15,
 }
---
+++ devnote/Mud Text Codes:.md
@@ -0,0 +1,32 @@
+Mud Text Codes:
+
+Text codes start with {{ and end with a letter, number or symbol.
+The style or color applied persists until turned off or until the end
+of the page. For styles, the style toggles on and off with the same code.
+Such as {{+bold{{+. Finally, you can reset the color AND style with: {{x
+To display a '{{' type {{{{
+
+ Background colors:       Styles:
+ {{! {!Black   {x              {{+ {+Bold{+
+ {{@ {@Red     {x              {{/ {/Italic{/
+ {{# {#Green   {x              {{_ {_Underline{_
+ {{$ {$Yellow  {x              {{= {=Inverse{=
+ {{% {%Blue    {x              {{- {-Strike-through{-
+ {{^ {^Magenta {x              {{x {xReset All
+ {{& {&Cyan    {x              {{n {xNew-line
+ {{* {k{*White   {x
+
+ Bright colors:           Normal colors:
+ {{K {KDark Gray{x             {{k {KBlack        {x
+ {{R {RRed      {x             {{r {rRed          {x
+ {{G {GGreen    {x             {{g {gGreen        {x
+ {{Y {YYellow   {x             {{y {yYellow       {x
+ {{B {BBlue     {x             {{b {bBlue         {x
+ {{M {MMagenta  {x             {{m {mMagenta      {x
+ {{C {CCyan     {x             {{c {cCyan         {x
+ {{W {WWhite    {x             {{w {wGray         {x
+
+For legibility, black is shown as dark gray above.
+BG and FG colors can be combined: {@{CRed BG, Cyan FG{x
+
+For EXTENDED COLORS: help XCOLOR
